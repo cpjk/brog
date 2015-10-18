2879a98d4fdec247dee67073f1eda3d69c33bd25
Chris Kelly <cpjkelly@gmail.com>
2015-10-18 14:17:54 -0700

    Add users

---
+++ priv/repo/migrations/20151018210431_create_user.exs
@@ -0,0 +1,13 @@
+defmodule Blog.Repo.Migrations.CreateUser do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :first_name, :string
+      add :last_name, :string
+
+      timestamps
+    end
+
+  end
+end
---
+++ web/views/user_view.ex
@@ -0,0 +1,3 @@
+defmodule Blog.UserView do
+  use Blog.Web, :view
+end
