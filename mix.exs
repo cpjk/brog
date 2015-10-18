70b867fb5c1ed38536d89ddb8edd2c2b610e3fb5
Chris Kelly <cpjkelly@gmail.com>
2015-10-18 15:47:55 -0700

    Add comeonin

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Blog.Mixfile do
   def application do
     [mod: {Blog, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :comeonin]]
   end
 
   # Specifies which paths to compile per environment.
@@ -35,7 +35,8 @@ defmodule Blog.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:comeonin, "~> 1.3"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
