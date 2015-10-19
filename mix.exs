c3610b47c6e118477b7ff965a53e5a2d738926dd
Chris Kelly <cpjkelly@gmail.com>
2015-10-18 20:39:03 -0700

    Add joken

--- mix.exs
+++ mix.exs
@@ -36,7 +36,8 @@ defmodule Blog.Mixfile do
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 1.3"}]
+     {:comeonin, "~> 1.3"},
+     {:joken, "~> 0.16.1"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
