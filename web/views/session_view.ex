ece7909ba56cd92eac1ff8caa7a36cb1e066291f
Chris Kelly <cpjkelly@gmail.com>
2015-10-26 23:15:04 -0700

    Improve UX

--- web/views/session_view.ex
+++ web/views/session_view.ex
@@ -4,4 +4,16 @@ defmodule Blog.SessionView do
   def has_errors?(changeset = %Ecto.Changeset{}) do
     !Enum.empty? changeset.errors
   end
+
+  def poop_emoji do
+    Exmoji.find_by_short_name("poop")
+    |> Enum.at(0)
+    |> Exmoji.EmojiChar.render
+  end
+
+  def thumbs_down do
+    Exmoji.find_by_short_name("-1")
+    |> Enum.at(0)
+    |> Exmoji.EmojiChar.render
+  end
 end
