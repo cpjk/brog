51f9b5f0f864a7c7cdb90ae2043bec907befd97b
Chris Kelly <cpjkelly@gmail.com>
2015-12-13 18:16:46 -0800

    Add comment

--- lib/abilities.ex
+++ lib/abilities.ex
@@ -3,7 +3,7 @@ defmodule Blog.Abilities do
   defimpl Canada.Can, for: Blog.User do
     def can?(%Blog.User{}, action, Blog.User) when action in [:index, :create], do: true
     def can?(%Blog.User{id: id}, _, %Blog.User{id: id}), do: true
-    def can?(%Blog.User{}, _, _), do: false
+    def can?(%Blog.User{}, _, _), do: false # catchall to ensure that a user can only do what we permit
   end
 
   defimpl Canada.Can, for: Atom do
