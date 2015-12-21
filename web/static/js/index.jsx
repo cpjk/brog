7f6feede6fbf9668edcbc89fe772c7c4fa8dc401
Chris Kelly <cpjkelly@gmail.com>
2015-12-21 14:29:10 -0800

    Make react and webpack work

---
+++ web/static/js/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx'
+
+main();
+
+function main() {
+  const app = document.createElement("div");
+
+  document.body.appendChild(app);
+
+  ReactDOM.render(<App />, app);
+};
