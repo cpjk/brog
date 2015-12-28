38a6829e860319467e9f17616a61604bf1de847d
Chris Kelly <cpjkelly@gmail.com>
2015-12-28 00:57:15 -0800

    Remove Comment.changeset/2

--- web/models/comment.ex
+++ web/models/comment.ex
@@ -17,11 +17,6 @@ defmodule Blog.Comment do
   If no params are provided, an invalid changeset is returned
   with no validation performed.
   """
-  def changeset(model, params \\ :empty) do
-    model
-    |> cast(params, @required_fields, @optional_fields)
-  end
-
   def create_changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
