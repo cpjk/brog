117cb512300d2b8534e72254f0f56aaa3f5f62c7
Chris Kelly <cpjkelly@gmail.com>
2016-01-05 12:51:14 -0800

    Update UserController tests

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -2,12 +2,21 @@ defmodule Blog.UserControllerTest do
   use Blog.ConnCase
 
   alias Blog.User
-  @valid_attrs %{first_name: "some content",
-    last_name: "some content",
+
+  @valid_new_user_attrs %{first_name: "Morty",
+    last_name: "Smith",
+    email: "morty@earth.com",
+    password: "ohgeeze"}
+  @valid_stored_user_attrs Map.delete(@valid_new_user_attrs, :password)
+
+  @valid_current_user_attrs %{first_name: "Rick",
+    last_name: "Sanchez",
     email: "ricksanchez@earth.com",
     password: "rickytickytavy"}
-  @valid_stored_attrs Map.delete(@valid_attrs, :password)
+  @valid_login_attrs %{email: "ricksanchez@earth.com",
+    password: "rickytickytavy"}
   @invalid_attrs %{}
+
 
   setup do
     conn = conn()
@@ -15,6 +24,9 @@ defmodule Blog.UserControllerTest do
   end
 
   test "lists all entries on index", %{conn: conn} do
+    TestHelpers.create_user(@valid_current_user_attrs)
+    conn = TestHelpers.login_user(conn, @valid_login_attrs)
+
     conn = get conn, user_path(conn, :index)
     assert html_response(conn, 200) =~ "Listing users"
   end
@@ -25,45 +37,62 @@ defmodule Blog.UserControllerTest do
   end
 
   test "creates resource and redirects when data is valid", %{conn: conn} do
-    conn = post conn, user_path(conn, :create), user: @valid_attrs
+    TestHelpers.create_user(@valid_current_user_attrs)
+    conn = TestHelpers.login_user(conn, @valid_login_attrs)
+
+    conn = post conn, user_path(conn, :create), user: @valid_new_user_attrs
     assert redirected_to(conn) == user_path(conn, :index)
-    assert Repo.get_by(User, @valid_stored_attrs)
+    assert Repo.get_by(User, @valid_stored_user_attrs)
   end
 
   test "does not create resource and renders errors when data is invalid", %{conn: conn} do
+    TestHelpers.create_user(@valid_current_user_attrs)
+    conn = TestHelpers.login_user(conn, @valid_login_attrs)
+
     conn = post conn, user_path(conn, :create), user: @invalid_attrs
     assert html_response(conn, 200) =~ "New user"
   end
 
   test "shows chosen resource", %{conn: conn} do
+    TestHelpers.create_user(@valid_current_user_attrs)
+    conn = TestHelpers.login_user(conn, @valid_login_attrs)
+
     user = Repo.insert! %User{}
     conn = get conn, user_path(conn, :show, user)
     assert html_response(conn, 200) =~ "Show user"
   end
 
   test "renders page not found when id is nonexistent", %{conn: conn} do
-    assert_raise Ecto.NoResultsError, fn ->
-      get conn, user_path(conn, :show, -1)
-    end
+    conn = get conn, user_path(conn, :show, -1)
+    assert html_response(conn, 302) =~ "/"
   end
 
   test "renders form for editing chosen resource", %{conn: conn} do
-    user = Repo.insert! %User{}
-    conn = get conn, user_path(conn, :edit, user)
+    current_user = TestHelpers.create_user(@valid_current_user_attrs)
+    conn = TestHelpers.login_user(conn, @valid_login_attrs)
+
+    conn = get conn, user_path(conn, :edit, current_user)
     assert html_response(conn, 200) =~ "Edit user"
   end
 
   test "updates chosen resource and redirects when data is valid", %{conn: conn} do
-    user = Repo.insert! %User{}
-    conn = put conn, user_path(conn, :update, user), user: @valid_attrs
-    assert redirected_to(conn) == user_path(conn, :show, user)
-    assert Repo.get_by(User, @valid_stored_attrs)
+    current_user = TestHelpers.create_user(@valid_current_user_attrs)
+    conn = TestHelpers.login_user(conn, @valid_login_attrs)
+
+    new_attrs = Map.put(@valid_current_user_attrs, :first_name, "James")
+    |> Map.delete(:password)
+
+    conn = put conn, user_path(conn, :update, current_user), user: new_attrs
+    assert redirected_to(conn) == user_path(conn, :show, current_user)
+    assert Repo.get_by(User, new_attrs)
   end
 
   test "deletes chosen resource", %{conn: conn} do
-    user = Repo.insert! %User{}
-    conn = delete conn, user_path(conn, :delete, user)
+    current_user = TestHelpers.create_user(@valid_current_user_attrs)
+    conn = TestHelpers.login_user(conn, @valid_login_attrs)
+
+    conn = delete conn, user_path(conn, :delete, current_user)
     assert redirected_to(conn) == user_path(conn, :index)
-    refute Repo.get(User, user.id)
+    refute Repo.get(User, current_user.id)
   end
 end
